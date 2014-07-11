df38d29929d3b3e3b896583c33bea8a1af3146f2
David Stinemetze <davids@widgetsandburritos.com>
2014-07-11 00:25:25 -0500

    added $book1 $book2 & $book3

---
+++ lesson1/1-multidimensionalarrays.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Multi-dimensional Arrays Explained
+ */
+
+
+
+// standard associative array
+$book1 = array(
+    'title' => 'The Fantastic Light', 'author' => 'Terry Pratchett', 'year' => 1997
+);
+
+$book2 = array(
+    'title' => 'Daemon', 'author' => 'Daniel Suarez', 'year' => 2006
+);
+
+$book3 = array(
+    'title' => 'Eric', 'author' => 'Terry Pratchett', 'year' => 1990
+);
+
+
